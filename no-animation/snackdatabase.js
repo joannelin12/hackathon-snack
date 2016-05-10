d17aaa0919a5e9a5e1a03dead6df654b22a25c5e
Joanne Lin <joannelin12@gmail.com>
2016-05-09 22:25:09 -0700

    no animation

---
+++ no-animation/snackdatabase.js
@@ -0,0 +1,193 @@
+        var snackSource=[
+
+        // POPCORN
+        {
+            snack: "Salted Caramel Apple Popcorn",
+            photo: "https://naturebox.com/resize/275/215/crop/h/media/catalog/product/1/3/1388_snackcard.jpg",
+            taste: "sweet",
+            texture: "crunchy"
+        },
+        {
+            snack: "Coffee Kettle Popcorn",
+            photo: "https://naturebox.com/resize/275/215/crop/h/media/catalog/product/5/1/518_snackcard.jpg",
+            taste: "sweet",
+            texture: "crunchy"
+        },
+        {
+            snack: "Parmesan Garlic Pops Pops",
+            photo: "https://naturebox.com/resize/275/215/crop/h/media/catalog/product/6/5/658_snackcard.jpg",
+            taste: "salty",
+            texture: "crunchy"
+        },
+        {
+            snack: "Sea Salt Pop Pops",
+            photo: "https://naturebox.com/resize/275/215/crop/h/media/catalog/product/5/4/544_snackcard.jpg",
+            taste: "salty",
+            texture: "crunchy"
+        },
+        {
+            snack: "Spicy Sriracha Popcorn",
+            photo: "https://naturebox.com/resize/275/215/crop/h/media/catalog/product/1/3/1325_snackcard.jpg",
+            taste: "spicy",
+            texture: "crunchy"
+        },
+        {
+            snack: "Jalapeno White Cheddar Popcorn",
+            photo: "https://naturebox.com/resize/275/215/crop/h/media/catalog/product/7/0/709_snackcard.jpg",
+            taste: "spicy",
+            texture: "crunchy"
+        },
+
+        // Chips and stuff
+        {
+            snack: "Honey Dijon Pretzels",
+            photo: "https://naturebox.com/resize/275/215/crop/h/media/catalog/product/4/7/470_snackcard_1.jpg",
+            taste: "sweet",
+            texture: "crunchy"
+        },
+        {
+            snack: "Blueberry Greek Yogurt Pretzels",
+            photo: "https://naturebox.com/resize/275/215/crop/h/media/catalog/product/6/4/648_snackcard.jpg",
+            taste: "sweet",
+            texture: "crunchy"
+        },
+        {
+            snack: "Honey Crunch Crisps",
+            photo: "https://naturebox.com/resize/275/215/crop/h/media/catalog/product/3/4/341_snackcard.jpg",
+            taste: "sweet",
+            texture: "crunchy"
+        },
+        {
+            snack: "Toasted Seasame Stix",
+            photo: "https://naturebox.com/resize/275/215/crop/h/media/catalog/product/2/3/232_snackcard.jpg",
+            taste: "salty",
+            texture: "crunchy"
+        },
+        {
+            snack: "Cranberry Pepita Crisps",
+            photo: "https://naturebox.com/resize/275/215/crop/h/media/catalog/product/6/0/605_snackcard.jpg",
+            taste: "sweet",
+            texture: "crunchy"
+        },
+        {
+            snack: "Honey Macadamia Pretzel Pops",
+            photo: "https://naturebox.com/resize/275/215/crop/h/media/catalog/product/4/7/472_snackcard.jpg",
+            taste: "sweet",
+            texture: "crunchy"
+        },
+        {
+            snack: "Kung Pao Pretzels",
+            photo: "https://naturebox.com/resize/275/215/crop/h/media/catalog/product/5/8/585_snackcard.jpg",
+            taste: "spicy",
+            texture: "crunchy"
+        },
+        {
+            snack: "Aged Cheddar Lentil Loops",
+            photo: "https://naturebox.com/resize/275/215/crop/h/media/catalog/product/1/3/1381_snackcard.jpg",
+            taste: "salty",
+            texture: "crunchy"
+        },
+        {
+            snack: "Santa Fe Corn Stix",
+            photo: "https://naturebox.com/resize/275/215/crop/h/media/catalog/product/3/8/382_snackcard.jpg",
+            taste: "salty",
+            texture: "crunchy"
+        },
+        {
+            snack: "Salt and Pepper Lentil Loops",
+            photo: "https://naturebox.com/resize/275/215/crop/h/media/catalog/product/5/4/543_snackcard.jpg",
+            taste: "salty",
+            texture: "crunchy"
+        },
+        {
+            snack: "Masa Crisps",
+            photo: "https://naturebox.com/resize/275/215/crop/h/media/catalog/product/1/5/151_snackcard.jpg",
+            taste: "salty",
+            texture: "crunchy"
+        },
+        {
+            snack:"Garlic Bread Cheese Crisps",
+            photo:"https://naturebox.com/resize/275/215/crop/h/media/catalog/product/1/3/1337_snackcard.jpg",
+            taste: "salty",
+            texture: "crunchy"
+        },
+        {
+            snack:"Salt & Vinegar Veggie Chips",
+            photo:"https://naturebox.com/resize/275/215/crop/h/media/catalog/product/1/3/1382_site3.jpg",
+            taste: "salty",
+            texture: "crunchy"
+        },
+
+
+
+        //Dried Fruit & Chews
+        {
+            snack:"Double Berry Fruit Peels",
+            photo:"https://naturebox.com/resize/275/215/crop/h/media/catalog/product/8/2/820_snackcard_morefill.jpg",
+            taste: "sweet",
+            texture: "soft"
+        },
+        {
+            snack:"Granny Smith Apples",
+            photo:"https://naturebox.com/resize/275/215/crop/h/media/catalog/product/3/1/318_snackcard.jpg",
+            taste: "sweet",
+            texture: "soft"
+        },
+        {
+            snack:"Dried White Peaches",
+            photo:"https://naturebox.com/resize/275/215/crop/h/media/catalog/product/1/4/1433_snackcard.jpg",
+            taste: "sweet",
+            texture: "soft"
+        },
+        {
+            snack:"Dried Mango",
+            photo:"https://naturebox.com/resize/275/215/crop/h/media/catalog/product/1/0/109_snackcard.jpg",
+            taste: "sweet",
+            texture: "soft"
+        },
+        {
+            snack:"Fuji Apples",
+            photo:"https://naturebox.com/resize/275/215/crop/h/media/catalog/product/4/6/466_snackcard.jpg",
+            taste: "sweet",
+            texture: "soft"
+        },
+        {
+            snack:"Dried Pears",
+            photo:"https://naturebox.com/resize/275/215/crop/h/media/catalog/product/1/4/142_snackcard.jpg",
+            taste: "sweet",
+            texture: "soft"
+        },
+        {
+            snack:"Dried California Peaches",
+            photo:"https://naturebox.com/resize/275/215/crop/h/media/catalog/product/3/2/328_snackcard.jpg",
+            taste: "sweet",
+            texture: "soft"
+        },
+        {
+            snack:"Cherry Berry Bonanza",
+            photo:"https://naturebox.com/resize/275/215/crop/h/media/catalog/product/3/5/356_snackcard.jpg",
+            taste: "sweet",
+            texture: "soft"
+        },
+
+        // Other
+        {
+            snack:"Crispy Coconut Squares",
+            photo:"https://naturebox.com/resize/275/215/crop/h/media/catalog/product/1/3/1391_snackcard.jpg",
+            taste:"sweet",
+            texture: "crunchy"
+        },
+        {
+            snack:"No Results Found ): Try again!",
+            photo:"",
+            taste:"spicy",
+            texture: "soft"
+        },
+        {
+            snack:"No Results Found ): Try again!",
+            photo:"",
+            taste:"salty",
+            texture: "soft"
+        },
+
+        ];
